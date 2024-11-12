a31d4fe64c6ece3a4d9d6f903751c2fd3cc8e540
Pe7r0v14 <davydovpetrvladimirovich@gmail.com>
2024-11-12 13:28:26 +0800

    I step 2 ready si

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+from  brain_games.cli import welcome_user
+#1 Hello
+#2 Cycle start-finish
+#3 Question
+#4 Enter user
+#5 'Excellent!'
+
 
 def main():
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 if __name__=='__main__':
     main()
